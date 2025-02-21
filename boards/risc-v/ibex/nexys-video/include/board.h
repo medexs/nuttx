84688546ee0655241a1b191a59a6b2acf08439f3
Jan Medek <czmedexs@gmail.com>
2025-02-21 11:17:47 +0100

    feat(boards/risc-v/ibex/nexys-video/): add support for Nexys Video board

---
+++ boards/risc-v/ibex/nexys-video/include/board.h
@@ -0,0 +1,50 @@
+/****************************************************************************
+ * boards/risc-v/ibex/nexys-video/include/board.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __BOARDS_RISCV_IBEX_NEXYS_VIDEO_INCLUDE_BOARD_H
+#define __BOARDS_RISCV_IBEX_NEXYS_VIDEO_INCLUDE_BOARD_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+#include <stdint.h>
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+// Nexys Video LEDs
+#define BOARD_LED_CNT       8
+#define BOARD_ALL_LED_MASK  ((BOARD_LED_CNT == 32) ? 0xFFFFFFFF : (((uint32_t)1 << BOARD_LED_CNT) - 1))
+#define BOARD_LED0_MASK     0x1
+#define BOARD_LED_MASK(n)   (BOARD_LED0_MASK << n)
+
+// NuttX Event LEDs
+#define LED_CPU             BOARD_LED_MASK(0)
+//  #define LED_STARTED         BOARD_LED_MASK(1)
+#define LED_HEAPALLOCATE    BOARD_LED_MASK(1)
+#define LED_IRQSENABLED     BOARD_LED_MASK(2)
+#define LED_STACKCREATED    BOARD_LED_MASK(3)
+#define LED_INIRQ           BOARD_LED_MASK(4)
+#define LED_SIGNAL          BOARD_LED_MASK(5)
+#define LED_ASSERTION       BOARD_LED_MASK(6)
+#define LED_PANIC           BOARD_LED_MASK(7)
+
+#endif /* __BOARDS_RISCV_IBEX_NEXYS_VIDEO_INCLUDE_BOARD_H */
